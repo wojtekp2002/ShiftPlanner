93fcd9df2d0ddf0257ba3fc873f501364c30de38
wojtek pasieka <wojtek.pasiu@gmail.com>
2026-05-13 10:10:39 +0200

    Configure Supabase clients

---
+++ web/src/lib/superbase/server.ts
@@ -0,0 +1,31 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Missing Supabase environment variables");
+  }
+
+  return createServerClient(supabaseUrl, supabaseAnonKey, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
+        } catch {
+          // This can happen when called from a Server Component.
+          // Middleware will refresh user sessions.
+        }
+      },
+    },
+  });
+}
